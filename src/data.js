1b01a0cd0cd922aa9b4ba5a78a17323753579bb6
workwithraymond <workwithraymond01@gmail.com>
2023-12-13 19:47:56 -0500

    second commit

--- src/data.js
+++ src/data.js
@@ -23,8 +23,8 @@ export const service = [
 ];
 
 export const tours = [
-    {id: 1, image: tour1, date: 'august 26th, 2020',title: 'Tibet Adventure', info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', location: 'China', duration: 6, cost: 2100},
-    {id: 2, image: tour2, date: 'october 1th, 2020',title: 'best of java', info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', location: 'indonesia', duration: 11, cost: 1400},
-    {id: 3, image: tour3, date: 'september 15th, 2020',title: 'Tibet Adventure', info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', location: 'explore hong kong', duration: 8, cost: 5000},
-    {id: 4, image: tour4, date: 'december 5th, 2019',title: 'kenya highlights', info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', location: ' kenya', duration: 20, cost: 3300},
+    {id: 1, image: tour1, date: 'august 26th, 2023',title: 'Tibet Adventure', info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', location: 'China', duration: 6, cost: 2100},
+    {id: 2, image: tour2, date: 'october 1th, 2023',title: 'best of java', info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', location: 'indonesia', duration: 11, cost: 1400},
+    {id: 3, image: tour3, date: 'september 15th, 2023',title: 'Tibet Adventure', info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', location: 'explore hong kong', duration: 8, cost: 5000},
+    {id: 4, image: tour4, date: 'december 5th, 2022',title: 'kenya highlights', info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', location: ' kenya', duration: 20, cost: 3300},
 ]
